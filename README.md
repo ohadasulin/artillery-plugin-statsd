9989d5ce5a46113777ff377331f4600c80f6f4b7
ohad <ohad.asulin@gmail.com>
2017-12-10 13:44:45 +0200

    changed statsd to Telegraf and added tagging support

--- README.md
+++ README.md
@@ -1,15 +1,15 @@
-# StatsD output for Artillery stats
+# Telegraf output for Artillery stats
 
 This [Artillery](https://artillery.io/) plugin allows you to publish the
-stats produced by Artillery CLI to StatsD in real-time.
+stats produced by Artillery CLI to [Telegraf](https://github.com/influxdata/telegraf)'s StatsD input plugin in real-time.
 
 ## Usage
 
 ### Install
 
-`npm install -g artillery-plugin-statsd` - if you've installed Artillery globally
+`npm install -g artillery-plugin-telegraf` - if you've installed Artillery globally
 
-`npm install artillery-plugin-statsd` otherwise.
+`npm install artillery-plugin-telegraf` otherwise.
 
 ### Use
 
@@ -23,7 +23,11 @@ Enable the plugin by adding it in your test script's `config.plugins` section:
       "statsd": {
         "host": "localhost",
         "port": 8125,
-        "prefix": "artillery"
+        "prefix": "artillery",
+        "tags" : {
+            "testName": "myTestName",
+            "testId": "myTestId"
+        }
       }
     }
   }
@@ -32,6 +36,8 @@ Enable the plugin by adding it in your test script's `config.plugins` section:
 ```
 
 `host`, `port`, and `prefix` are optional; the values above are the defaults.
+
+`tags` is optional - used to tag metrics if [Telegraf](https://github.com/influxdata/telegraf) reports to [InfluxDB](https://www.influxdata.com/).
 
 ### Published metrics
 
@@ -71,32 +77,7 @@ Metrics can be skipped by passing in an additional configuration property `skipL
 - `"skipList": "latency.max"` - would skip only the `latency.max` metric
 - `"skipList": "scenarioDuration, latency.max"` - a comma separated list can be used to pass in multiple values.
 
-### Using with Librato
 
-This plugin can be used to publish metrics to [Librato](https://www.librato.com):
-
-0. Install StatsD with:
-
-  `npm install statsd`
-
-1. Add Librato backend to StatsD:
-
-  `cd /path/to/statsd`
-  `npm install statsd-librato-backend`
-
-  Enable the backend in your StatsD config:
-
-  ```javascript
-  {
-    librato: {
-      email:  "mylibrato@email.com",
-      token:  "a161e2bc22b1bdd0cfe90412token10498token22dd52cat792doge1ab5a1d32"
-    },
-    backends: ['statsd-librato-backend']
-  }
-  ```
-
-3. Run StatsD and use Artillery with this plugin.
 
 ## License
 
--- index.js
+++ index.js
@@ -1,126 +1,131 @@
 'use strict';
 
-var Lynx = require('lynx');
-var l = require('lodash');
-var debug = require('debug')('plugins:statsd');
+const Lynx = require('lynx');
+const _ = require('lodash');
+const debug = require('debug')('plugins:telegraf');
 
-function StatsDPlugin(rawConfig, ee) {
-  var self = this;
-  self._report = [];
+function TelegrafPlugin(rawConfig, ee) {
+    const self = this;
+    self._report = [];
 
-  var config = _reconcileConfigs(rawConfig);
-  debug('Resulting StatsD Configuration: '+JSON.stringify(config));
+    const config = _reconcileConfigs(rawConfig);
+    debug('Resulting StatsD Configuration: ' + JSON.stringify(config));
 
-  var metrics = new Lynx(config.host, config.port);
+    const metrics = new Lynx(config.host, config.port);
 
-  ee.on('stats', function(statsObject) {
-    var stats = statsObject.report()
-    debug('Stats Report from Artillery: '+JSON.stringify(stats));
+    ee.on('stats', function (statsObject) {
+        const stats = statsObject.report();
+        debug('Stats Report from Artillery: ' + JSON.stringify(stats));
 
-    if (config.enableUselessReporting) {
-      self._report.push({ timestamp: stats.timestamp, value: 'test' });
-    }
+        if (config.enableUselessReporting) {
+            self._report.push({ timestamp: stats.timestamp, value: 'test' });
+        }
 
-    var flattenedStats = _flattenStats('',stats, config.skipList, config.defaultValue);
-    debug('Flattened Stats Report: '+JSON.stringify(flattenedStats));
+        const flattenedStats = _flattenStats('', stats, config.skipList, config.defaultValue);
+        debug('Flattened Stats Report: ' + JSON.stringify(flattenedStats));
 
-    l.each(flattenedStats, function(value, name) {
-      debug('Reporting: '+name+'  '+value)
-      metrics.gauge(config.prefix+'.'+name, value || config.defaultValue);
+        _.each(flattenedStats, function (value, name) {
+            debug('Reporting: ' + name + '  ' + value);
+            const tagString = _.reduce(config.tags, (result, tagValue, tagName) => {
+                result += ',' + tagName + "=" + tagValue;
+                return result;
+            }, "");
+            metrics.gauge(config.prefix + '.' + name + tagString, value || config.defaultValue);
+        });
+
     });
 
-  });
+    ee.on('done', function () {
+        debug('done');
+        if (config.closingTimeout > 0) {
+            setTimeout(function () {
+                metrics.close();
+            }, config.closingTimeout);
+        } else {
+            metrics.close();
+        }
+    });
 
-  ee.on('done', function(stats) {
-    debug('done');
-    if (config.closingTimeout > 0) {
-      setTimeout(function () {
-        metrics.close();
-      }, config.closingTimeout);
-    } else {
-      metrics.close();
-    }
-  });
-
-  return this;
+    return this;
 }
 
 
-StatsDPlugin.prototype.report = function report() {
-  if (this._report.length === 0) {
-    return null;
-  } else {
-    this._report.push({
-      timestamp: 'aggregate',
-      value: {test: 'aggregate test'}
-    });
-    return this._report;
-  }
+TelegrafPlugin.prototype.report = function report() {
+    if (this._report.length === 0) {
+        return null;
+    } else {
+        this._report.push({
+                              timestamp: 'aggregate',
+                              value: { test: 'aggregate test' }
+                          });
+        return this._report;
+    }
 };
 
 // Parses the stats object and sub objects to gauge stats
-function _flattenStats(prefix, value, skipList, defaultValue){
-  var flattenedStats = {};
-  // Skip logic
-  if(l.contains(skipList, prefix)){
-    debug(prefix+' skipped');
-    return {};
-  }
+function _flattenStats(prefix, value, skipList, defaultValue) {
+    let flattenedStats = {};
+    // Skip logic
+    if (_.contains(skipList, prefix)) {
+        debug(prefix + ' skipped');
+        return {};
+    }
 
-  // Recursively loop through objects with sub values such as latency/errors
-  if(l.size(value) > 0){
-    l.each(value, function(subValue, subName) {
-      var newPrefix = prefix;
-      if(newPrefix==='') {
-        newPrefix = subName;
-      }
-      else {
-        newPrefix += '.'+subName;
-      }
-      flattenedStats =  l.merge(flattenedStats, _flattenStats(newPrefix, subValue, skipList, defaultValue));
-    });
-  }
-  // Hey, it is an actual stat!
-  else if(l.isFinite(value)){
-    flattenedStats = l.merge(flattenedStats,{[prefix]: value});
-  }
-  // Artillery is sending null or NaN.
-  else if(l.isNaN(value) || l.isNull(value)){
-    flattenedStats = l.merge(flattenedStats,{[prefix]: defaultValue});
-  }
-  // Empty object such as 'errors' when there are not actually errors
-  else{
-    debug(prefix+' has nothing to report');
-    // no-op
-  }
-  return flattenedStats;
+    // Recursively loop through objects with sub values such as latency/errors
+    if (_.size(value) > 0) {
+        _.each(value, function (subValue, subName) {
+            let newPrefix = prefix;
+            if (newPrefix === '') {
+                newPrefix = subName;
+            }
+            else {
+                newPrefix += '.' + subName;
+            }
+            flattenedStats = _.merge(flattenedStats, _flattenStats(newPrefix, subValue, skipList, defaultValue));
+        });
+    }
+    // Hey, it is an actual stat!
+    else if (_.isFinite(value)) {
+        flattenedStats = _.merge(flattenedStats, { [prefix]: value });
+    }
+    // Artillery is sending null or NaN.
+    else if (_.isNaN(value) || _.isNull(value)) {
+        flattenedStats = _.merge(flattenedStats, { [prefix]: defaultValue });
+    }
+    // Empty object such as 'errors' when there are not actually errors
+    else {
+        debug(prefix + ' has nothing to report');
+        // no-op
+    }
+    return flattenedStats;
 }
 
-function _generateSkipList(input){
-  let skipList = ['timestamp', 'latencies']; //always skip these
+function _generateSkipList(input) {
+    let skipList = ['timestamp', 'latencies']; //always skip these
 
-  // Add any values passed in by the user
-  if (l.isString(input)){
-    let inputWithoutSpaces = input.replace(/\s/g,'');
-    skipList = skipList.concat(inputWithoutSpaces.split(','));
-  }
-  return skipList;
+    // Add any values passed in by the user
+    if (_.isString(input)) {
+        let inputWithoutSpaces = input.replace(/\s/g, '');
+        skipList = skipList.concat(inputWithoutSpaces.split(','));
+    }
+    return skipList;
 }
 
-function _reconcileConfigs(config){
-  return {
-     host: config.plugins.statsd.host || 'localhost',
-     port: config.plugins.statsd.port || 8125,
-     prefix: config.plugins.statsd.prefix || 'artillery',
-     closingTimeout: config.plugins.statsd.timeout || 0,
-     defaultValue: config.plugins.statsd.default || 0,
-     skipList: _generateSkipList(config.plugins.statsd.skipList),
-    // This is used for testing the plugin interface
-     enableUselessReporting: config.plugins.statsd.enableUselessReporting
-  }
+function _reconcileConfigs(config) {
+    return {
+        host: config.plugins.telegraf.host || 'localhost',
+        port: config.plugins.telegraf.port || 8125,
+        prefix: config.plugins.telegraf.prefix || 'artillery',
+        closingTimeout: config.plugins.telegraf.timeout || 0,
+        defaultValue: config.plugins.telegraf.default || 0,
+        skipList: _generateSkipList(config.plugins.telegraf.skipList),
+        // This is used for testing the plugin interface
+        enableUselessReporting: config.plugins.telegraf.enableUselessReporting,
+        tags: config.plugins.telegraf.tags
+    }
 }
 
-module.exports = StatsDPlugin;
+module.exports = TelegrafPlugin;
 
 // Exported for testing purposes...
 module.exports._generateSkipList = _generateSkipList;
